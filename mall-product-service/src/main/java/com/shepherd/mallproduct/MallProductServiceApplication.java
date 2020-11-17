f0f94545138b0b2c4e35e35030f4c54c67dc76d4
shepherd <1059959730@qq.com>
2020-11-18 00:43:53 +0800

    update：进行服务注册到注册中心

--- mall-product-service/src/main/java/com/shepherd/mallproduct/MallProductServiceApplication.java
+++ mall-product-service/src/main/java/com/shepherd/mallproduct/MallProductServiceApplication.java
@@ -3,9 +3,11 @@ package com.shepherd.mallproduct;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableEurekaClient
 @MapperScan(basePackages = "com.shepherd.mallproduct.dao")
 @ComponentScan(basePackages = {"com.shepherd"})
 public class MallProductServiceApplication {
