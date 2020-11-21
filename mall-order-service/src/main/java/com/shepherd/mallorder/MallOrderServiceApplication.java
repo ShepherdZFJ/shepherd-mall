ed61525a2bb341ebc1ce1a9933853d5ee72111e6
shepherd <zhengfj@isyscore.com>
2020-11-21 18:08:55 +0800

    add：添加order服务

---
+++ mall-order-service/src/main/java/com/shepherd/mallorder/MallOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shepherd.mallorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallOrderServiceApplication.class, args);
+    }
+
+}
---
+++ mall-order-service/src/test/java/com/shepherd/mallorder/MallOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shepherd.mallorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
