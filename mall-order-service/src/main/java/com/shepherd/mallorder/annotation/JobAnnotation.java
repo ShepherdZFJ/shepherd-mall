bc600cca2fa39d24c86a306affc1acd2da37911d
fjzheng <1059959730@qq.com>
2021-05-26 10:56:54 +0800

    add：添加quartz集群定时任务demo

---
+++ mall-order-service/src/main/java/com/shepherd/mallorder/annotation/JobAnnotation.java
@@ -0,0 +1,28 @@
+package com.shepherd.mallorder.annotation;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface JobAnnotation {
+
+    /**作业名称*/
+    String jobName() default "";
+
+    /**分组名称*/
+    String groupName() default "";
+
+    /**定时任务cron表达式*/
+    String cron() default "";
+
+    /**作业回调参数*/
+    String parameter() default "";
+
+    /**作业描述*/
+    String content() default "";
+
+}
---
+++ mall-order-service/src/main/java/com/shepherd/mallorder/factory/QuartzJobFactory.java
@@ -0,0 +1,25 @@
+package com.shepherd.mallorder.factory;
+
+import org.quartz.spi.TriggerFiredBundle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.scheduling.quartz.SpringBeanJobFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author fjzheng
+ * @version 1.0
+ * @date 2021/5/20 18:36
+ */
+@Component
+public   class QuartzJobFactory  extends SpringBeanJobFactory {
+    @Autowired
+    private AutowireCapableBeanFactory capableBeanFactory;
+
+    @Override
+    protected Object createJobInstance(TriggerFiredBundle bundle) throws Exception {
+        Object jobInstance = super.createJobInstance(bundle);
+        capableBeanFactory.autowireBean(jobInstance);
+        return jobInstance;
+    }
+}
