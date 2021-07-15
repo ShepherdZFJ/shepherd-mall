95a927439186ed487e4f8665001fdcd4cf9b3648
fjzheng <1059959730@qq.com>
2021-07-15 23:17:08 +0800

    update: 网关暂时去掉springSecurity框架鉴权，后期再改造升级为此框架

--- mall-gateway/src/main/java/com/shepherd/mallgateway/component/RestAuthenticationEntryPoint.java
+++ mall-gateway/src/main/java/com/shepherd/mallgateway/component/RestAuthenticationEntryPoint.java
@@ -8,8 +8,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.server.ServerAuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -19,17 +17,17 @@ import java.nio.charset.Charset;
 /**
  * 自定义返回结果：没有登录或token过期时
  */
-@Component
-public class RestAuthenticationEntryPoint implements ServerAuthenticationEntryPoint {
-    @Override
-    public Mono<Void> commence(ServerWebExchange exchange, AuthenticationException e) {
-        ServerHttpResponse response = exchange.getResponse();
-        response.setStatusCode(HttpStatus.OK);
-        response.getHeaders().set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-        response.getHeaders().set("Access-Control-Allow-Origin","*");
-        response.getHeaders().set("Cache-Control","no-cache");
-        String body= JSONUtil.toJsonStr(ResponseVO.failure(ResponseStatusEnum.UNAUTHORIZED, e.getMessage()));
-        DataBuffer buffer =  response.bufferFactory().wrap(body.getBytes(Charset.forName("UTF-8")));
-        return response.writeWith(Mono.just(buffer));
-    }
+//@Component
+public class RestAuthenticationEntryPoint  {
+//    @Override
+//    public Mono<Void> commence(ServerWebExchange exchange, AuthenticationException e) {
+//        ServerHttpResponse response = exchange.getResponse();
+//        response.setStatusCode(HttpStatus.OK);
+//        response.getHeaders().set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+//        response.getHeaders().set("Access-Control-Allow-Origin","*");
+//        response.getHeaders().set("Cache-Control","no-cache");
+//        String body= JSONUtil.toJsonStr(ResponseVO.failure(ResponseStatusEnum.UNAUTHORIZED, e.getMessage()));
+//        DataBuffer buffer =  response.bufferFactory().wrap(body.getBytes(Charset.forName("UTF-8")));
+//        return response.writeWith(Mono.just(buffer));
+//    }
 }
--- mall-gateway/src/main/java/com/shepherd/mallgateway/filter/IgnoreUrlsRemoveJwtFilter.java
+++ mall-gateway/src/main/java/com/shepherd/mallgateway/filter/IgnoreUrlsRemoveJwtFilter.java
@@ -19,23 +19,23 @@ import java.util.List;
  * 白名单路径访问时需要移除JWT请求头
  */
 @Component
-public class IgnoreUrlsRemoveJwtFilter implements WebFilter {
-    @Autowired
-    private IgnoreUrlsConfig ignoreUrlsConfig;
-    @Override
-    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-        ServerHttpRequest request = exchange.getRequest();
-        URI uri = request.getURI();
-        PathMatcher pathMatcher = new AntPathMatcher();
-        //白名单路径移除JWT请求头
-        List<String> ignoreUrls = ignoreUrlsConfig.getUrls();
-        for (String ignoreUrl : ignoreUrls) {
-            if (pathMatcher.match(ignoreUrl, uri.getPath())) {
-                request = exchange.getRequest().mutate().header(AuthConstant.JWT_TOKEN_HEADER, "").build();
-                exchange = exchange.mutate().request(request).build();
-                return chain.filter(exchange);
-            }
-        }
-        return chain.filter(exchange);
-    }
+public class IgnoreUrlsRemoveJwtFilter  {
+//    @Autowired
+//    private IgnoreUrlsConfig ignoreUrlsConfig;
+//    @Override
+//    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+//        ServerHttpRequest request = exchange.getRequest();
+//        URI uri = request.getURI();
+//        PathMatcher pathMatcher = new AntPathMatcher();
+//        //白名单路径移除JWT请求头
+//        List<String> ignoreUrls = ignoreUrlsConfig.getUrls();
+//        for (String ignoreUrl : ignoreUrls) {
+//            if (pathMatcher.match(ignoreUrl, uri.getPath())) {
+//                request = exchange.getRequest().mutate().header(AuthConstant.JWT_TOKEN_HEADER, "").build();
+//                exchange = exchange.mutate().request(request).build();
+//                return chain.filter(exchange);
+//            }
+//        }
+//        return chain.filter(exchange);
+//    }
 }
