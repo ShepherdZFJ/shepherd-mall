fbee48ea9739737b6f6851bc111d20c3930c05a6
fjzheng <1059959730@qq.com>
2021-07-20 10:53:30 +0800

    update:去掉quartz框架测试集群定时任务demo

--- mall-order-service/src/main/java/com/shepherd/mallorder/config/QuartzBindOperationConfig.java
+++ mall-order-service/src/main/java/com/shepherd/mallorder/config/QuartzBindOperationConfig.java
@@ -1,45 +1,45 @@
-package com.shepherd.mallorder.config;
-
-
-import com.shepherd.mallorder.factory.QuartzJobFactory;
-import com.shepherd.mallorder.schedule.TestSchedule;
-import org.quartz.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-
-
-/**
- * @author fjzheng
- * @version 1.0
- * @date 2021/5/20 18:42
- */
-@Configuration
-public class QuartzBindOperationConfig {
-    private Logger logger = LoggerFactory.getLogger(getClass());
-    @Autowired
-    private QuartzJobFactory quartzJobFactory;
-    @Autowired
-    private Scheduler scheduler;
-
-    public void scheduleBind() {
-        try {
-            scheduler.setJobFactory(quartzJobFactory);
-            JobDetail tesJobDetail = JobBuilder.newJob(TestSchedule.class)
-                    .withIdentity("tesJob", "tesJob").withDescription("定时任务demo")
-                    .build();
-            CronScheduleBuilder tesJobCronScheduleBuilder = CronScheduleBuilder.cronSchedule("0/10 * * * * ?");
-            CronTrigger tesJobCronTrigger = TriggerBuilder.newTrigger()
-                    .withIdentity("tesJobTrigger", "tesJob")
-                    .withSchedule(tesJobCronScheduleBuilder).build();
-            scheduler.scheduleJob(tesJobDetail, tesJobCronTrigger);
-            scheduler.start();
-        } catch (SchedulerException e) {
-            // e.printStackTrace();
-        }
-
-    }
-
-}
-
+//package com.shepherd.mallorder.config;
+//
+//
+//import com.shepherd.mallorder.factory.QuartzJobFactory;
+//import com.shepherd.mallorder.schedule.TestSchedule;
+//import org.quartz.*;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//
+//
+///**
+// * @author fjzheng
+// * @version 1.0
+// * @date 2021/5/20 18:42
+// */
+//@Configuration
+//public class QuartzBindOperationConfig {
+//    private Logger logger = LoggerFactory.getLogger(getClass());
+//    @Autowired
+//    private QuartzJobFactory quartzJobFactory;
+//    @Autowired
+//    private Scheduler scheduler;
+//
+//    public void scheduleBind() {
+//        try {
+//            scheduler.setJobFactory(quartzJobFactory);
+//            JobDetail tesJobDetail = JobBuilder.newJob(TestSchedule.class)
+//                    .withIdentity("tesJob", "tesJob").withDescription("定时任务demo")
+//                    .build();
+//            CronScheduleBuilder tesJobCronScheduleBuilder = CronScheduleBuilder.cronSchedule("0/10 * * * * ?");
+//            CronTrigger tesJobCronTrigger = TriggerBuilder.newTrigger()
+//                    .withIdentity("tesJobTrigger", "tesJob")
+//                    .withSchedule(tesJobCronScheduleBuilder).build();
+//            scheduler.scheduleJob(tesJobDetail, tesJobCronTrigger);
+//            scheduler.start();
+//        } catch (SchedulerException e) {
+//            // e.printStackTrace();
+//        }
+//
+//    }
+//
+//}
+//
--- mall-order-service/src/main/java/com/shepherd/mallorder/schedule/TestSchedule.java
+++ mall-order-service/src/main/java/com/shepherd/mallorder/schedule/TestSchedule.java
@@ -5,21 +5,21 @@ package com.shepherd.mallorder.schedule;
  * @version 1.0
  * @date 2021/5/20 18:47
  */
-import java.util.Date;
-import org.quartz.DisallowConcurrentExecution;
-import org.quartz.Job;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import java.util.Date;
+//import org.quartz.DisallowConcurrentExecution;
+//import org.quartz.Job;
+//import org.quartz.JobExecutionContext;
+//import org.quartz.JobExecutionException;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 
-@DisallowConcurrentExecution
-public class TestSchedule implements Job{
-
-    private Logger logger = LoggerFactory.getLogger(getClass());
-
-    @Override
-    public void execute(JobExecutionContext context) throws JobExecutionException {
-        logger.info("*****"+new Date());
-    }
-}
+//@DisallowConcurrentExecution
+//public class TestSchedule implements Job{
+//
+//    private Logger logger = LoggerFactory.getLogger(getClass());
+//
+//    @Override
+//    public void execute(JobExecutionContext context) throws JobExecutionException {
+//        logger.info("*****"+new Date());
+//    }
+//}
