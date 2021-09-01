4abcefcf8a3459d7b4e2606bb359847b98b40159
fjzheng <1059959730@qq.com>
2021-09-01 10:17:46 +0800

    fix：解决统计返回string字符串时报错问题

--- mall-common/src/main/java/com/shepherd/mall/advice/ResponseResultBodyAdvice.java
+++ mall-common/src/main/java/com/shepherd/mall/advice/ResponseResultBodyAdvice.java
@@ -24,7 +24,9 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 import org.springframework.web.util.WebUtils;
 
+import javax.annotation.Resource;
 import java.lang.annotation.Annotation;
+import java.util.Objects;
 
 /**
  * @author fjZheng
@@ -34,6 +36,8 @@ import java.lang.annotation.Annotation;
 @RestControllerAdvice
 @Slf4j
 public class ResponseResultBodyAdvice implements ResponseBodyAdvice<Object> {
+    @Resource
+    private ObjectMapper objectMapper;
 
     private static final Class<? extends Annotation> ANNOTATION_TYPE = ResponseResultBody.class;
 
@@ -48,8 +52,14 @@ public class ResponseResultBodyAdvice implements ResponseBodyAdvice<Object> {
     /**
      * 当类或者方法使用了 @ResponseResultBody 就会调用这个方法
      */
+    @SneakyThrows
     @Override
     public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        //如果返回类型是string，那么springmvc是直接返回的，此时需要手动转化为json
+        if (body instanceof String) {
+            String value = objectMapper.writeValueAsString(ResponseVO.success(body));
+            return value;
+        }
         // 防止重复包裹的问题出现
         if (body instanceof ResponseVO) {
             return body;
